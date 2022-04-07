32adf5af0d2cfb4eed6bdf4dca9899a0322b8816
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-04-07 17:14:57 +0530

    Added solution - LeetHub

--- Kadane's Algorithm - GFG/kadanes-algorithm.java
+++ Kadane's Algorithm - GFG/kadanes-algorithm.java
@@ -36,19 +36,19 @@ class Solution{
     long maxSubarraySum(int arr[], int n){
         
         // Your code here
-       int currsum = arr[0];
-       int bestsum = arr[0];
-       
-       for(int i=1;i<n;i++)
-       {
-           currsum+=arr[i];
-           if(arr[i]>currsum)
-           {
-           currsum = arr[i];
-           }
-           bestsum = Math.max(currsum,bestsum);
-       }
-       return bestsum;
+        int currsum = arr[0];
+        int maxsum = arr[0];
+        
+        for(int i = 1 ; i < n ;i++){
+            currsum += arr[i];
+            if(arr[i] > currsum){
+                currsum = arr[i];
+            }
+            
+            maxsum = Math.max(currsum , maxsum);
+            
+        }
+        return maxsum;
         
     }
     
