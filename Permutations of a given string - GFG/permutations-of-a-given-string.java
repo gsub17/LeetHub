4d53ea6183b13e87ac696a67c1c2adb951c3742d
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-05-05 15:42:16 +0530

    Added solution - LeetHub

--- Permutations of a given string - GFG/permutations-of-a-given-string.java
+++ Permutations of a given string - GFG/permutations-of-a-given-string.java
@@ -30,7 +30,7 @@ class Solution {
     public List<String> find_permutation(String S) {
         // Code here
         List<String> ans = new ArrayList<>();
-        ans = permutation(S , 0 , S.length()-1,ans);
+        ans = permutation(S,0 , S.length()-1,ans);
         Collections.sort(ans);
         return ans;
     }
@@ -40,18 +40,17 @@ class Solution {
             ans.add(S);
             return ans;
         }
-        
-        for(int i = left ; i <= right ; i++){
-            String s1 = swap(S , left , i);
-            permutation(s1 , left+1 , right , ans);
-            String s2 = swap(S , left , i);
+        for(int i =left ; i <= right ; i++){
+            String s1 = swap(S,left,i);
+            permutation(s1 , left+1,right,ans);
+            String s2 = swap(S,left,i);
         }
         return ans;
     }
     
-    public String swap(String s , int i , int j){
+    public String swap(String S , int i , int j){
         char temp ;
-        char[] arr = s.toCharArray();
+        char[] arr = S.toCharArray();
         temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
