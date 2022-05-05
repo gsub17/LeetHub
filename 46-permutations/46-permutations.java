fcdffd28e560da5cf25906321ce087b70da1da55
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-05-05 15:55:19 +0530

    Time: 2 ms (65.45%), Space: 44.5 MB (61.57%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,29 +1,29 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        ans = permutation(nums , 0 , nums.length-1 , ans);
+        ans = permutation(nums , 0 , nums.length-1 ,ans );
         return ans;
     }
     
-    public List<List<Integer>> permutation(int[] nums , int left , int right , List<List<Integer>> ans){
+    public List<List<Integer>> permutation(int[] nums , int left , int right ,List<List<Integer>> ans){
         if(left == right){
-            List<Integer> ans2 = new ArrayList<>();
+            List<Integer> k = new ArrayList<>();
             for(int i = 0 ; i < nums.length ; i++){
-                ans2.add(nums[i]);
+                k.add(nums[i]);
             }
-            ans.add(ans2);
+            ans.add(k);
             return ans;
         }
         
-        for(int i = left ; i <= right ; i++ ){
-            int[] arr = swap(nums , left , i );
-            permutation(arr , left+1 , right , ans);
-            int[] ar2r = swap(nums , left , i );
+        for(int i = left ; i <= right ; i++){
+            int[] n1 = swap(nums , left , i);
+            permutation(nums , left+1 , right,ans);
+            int[] n2 = swap(nums , left , i);
         }
         return ans;
     }
     
-    public int[] swap(int[] nums , int i , int j ){
+    public int[] swap(int[]nums , int i , int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
