d7ab5bab2747cb4a8ff57e4eece20b0f7028fc3c
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-05-13 20:31:45 +0530

    Added solution - LeetHub

---
+++ Maximum product of two numbers - GFG/maximum-product-of-two-numbers.java
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().maxProduct(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    int maxProduct(int arr[], int n) {
+        // code here
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i = 0 ; i < n ;i++){
+            pq.add(arr[i]);
+        }
+     //   System.out.println(pq.toString());
+        int x = pq.peek();
+        pq.poll();
+        int y = pq.peek();
+        return x*y;
+    }
+}
