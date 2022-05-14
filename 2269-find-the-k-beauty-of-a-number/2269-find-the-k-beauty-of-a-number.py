e18ef62bf70fcad4af08b5013c45602b84a0b4bf
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-05-14 22:41:16 +0530

    Time: 34 ms (100.00%), Space: 13.8 MB (75.00%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.py
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.py
@@ -6,11 +6,8 @@ class Solution:
             x = str(num)
             y = x[i:i+k]
             l.append(int(y))
-        print(l)
         count = 0
         for i in range(0 , len(l),1):
-            y = l[i]
-            
-            if (y != 0 and num % y == 0):
+            if (l[i] != 0 and num % l[i] == 0):
                 count+=1
         return count
