63c8380654c1ba5b84846a162fdbc0e01f360cfe
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-05-15 10:22:50 +0530

    Time: 0 ms (100.00%), Space: 41 MB (68.05%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,14 +1,12 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        
-        return josephus(n,k)+1;
+        int x = josephus(n , k);
+        return x+1;
     }
-    
     public int josephus(int n , int k){
         if(n==1){
             return 0;
         }
-        
         return (josephus(n-1,k)+k)%n;
     }
 }
