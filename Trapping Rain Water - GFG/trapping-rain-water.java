234e5de6e9b43692e5ff71ef81ede78732a92cbf
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-05-15 12:15:31 +0530

    Added solution - LeetHub

--- Trapping Rain Water - GFG/trapping-rain-water.java
+++ Trapping Rain Water - GFG/trapping-rain-water.java
@@ -37,34 +37,26 @@ class Solution{
     // arr: input array
     // n: size of array
     // Function to find the trapped water between the blocks.
-    static long trappingWater(int arr[], int n) { 
-        //left aux array
+    static long trappingWater(int height[], int n) { 
+        // Your code here
+    
         int[] left_aux = new int[n];
-        left_aux[0] = arr[0];
+        left_aux[0] = height[0];
         for(int i = 1 ; i < n ; i++){
-            if(left_aux[i-1] > arr[i]){
-                left_aux[i] = left_aux[i-1];
-            }else{
-                left_aux[i] = arr[i];
-            }
+            left_aux[i] = Math.max(left_aux[i-1] , height[i]);
         }
         
         int[] right_aux = new int[n];
-        right_aux[n-1] = arr[n-1];
-        for(int j = n-2 ; j >= 0 ; j-- ){
-            if(right_aux[j+1] > arr[j]){
-                right_aux[j] = right_aux[j+1];
-            }else{
-                right_aux[j] = arr[j];
-            }
+        right_aux[n-1] = height[n-1];
+         for(int i = n-2 ; i >= 0 ; i--){
+            right_aux[i] = Math.max(right_aux[i+1] , height[i]);
         }
-        long sum = 0;
-        for(int  i = 0 ; i < n ; i++){
-            sum = Math.min(left_aux[i] , right_aux[i]) - arr[i] + sum;
+        
+        long water = 0;
+        for(int i = 0 ; i < n;i++){
+            water+= (Math.min(left_aux[i],right_aux[i]) - height[i]);
         }
-        return sum;
-        
-        
+        return water;
     } 
 }
 
