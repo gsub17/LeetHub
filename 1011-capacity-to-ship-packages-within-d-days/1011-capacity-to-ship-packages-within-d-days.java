2dfdccbbd3fef3adf4e423648293ef4bc3f250d5
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-05-15 12:56:02 +0530

    Time: 11 ms (67.23%), Space: 54.8 MB (8.88%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,37 +1,38 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-     int min = 0; // max value in the array
-     int max = 0; // sum of all weights
-        for(int i = 0 ; i < weights.length ;i++){
-            max += weights[i];
-            min = Math.max(weights[i],min);
+        int min = 0 , max =0;
+        for(int i:weights){
+            min = Math.max(i , min);
+            max+=i;
         }
-        int res = 0;
+        int ans = 0;
         while(min <= max){
             int mid = (min+max)/2;
             if(isFeasible(weights , mid , days)){
-                res = mid;
+                ans = mid;
                 max = mid -1;
             }else{
-                min = mid + 1;
+                min = mid +1;
             }
         }
-        return res;
+        return ans;
     }
     
-    public boolean isFeasible(int[] a , int mid , int k){
+    public boolean isFeasible(int[] weights , int mid , int days){
         int sum = 0 , day = 1;
-        for(int i = 0 ; i < a.length ;i++){
-            if(sum+a[i] > mid){
+        for(int i = 0 ; i < weights.length ;i++){
+            if(sum+weights[i] > mid){
                 day++;
-                sum = a[i];
+                sum = weights[i];
             }else{
-                sum+= a[i];
+                sum+=weights[i];
             }
         }
-        if(day <= k){
+        if(day <= days){
             return true;
         }
         return false;
     }
+    
+    
 }
