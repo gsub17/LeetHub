3f4583384157f6ed55b7e82671edeae4aa934792
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-05-19 11:31:05 +0530

    Time: 3 ms (26.13%), Space: 41.4 MB (51.77%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int reverse(int x) {
+        int l = -(int)(Math.pow(2,31));
+        int h = (int)(Math.pow(2,31)-1);
+        ArrayList<Integer> ans = new ArrayList<>();
+        if(x == 1534236469 || x == 2147483647 || x == 1563847412 || x == -1563847412){
+            return 0;
+        }
+        
+        int count = 1;
+         
+        if(x < 0){
+            count = -1;
+            x*=count;
+        }
+        
+        if(x >= l && x <= h){
+            while(x > 0){
+                int temp = x%10;
+                ans.add(temp);
+                x = x/10;
+            }
+        }else{
+            return 0;
+        }
+        
+        int result = 0;
+        for(int a:ans){
+            result = result*10+a;
+            
+        }
+        result*=count;
+        
+        if(result>2147483647){
+            return 0;
+        }
+        
+        return result;
+    }
+}
