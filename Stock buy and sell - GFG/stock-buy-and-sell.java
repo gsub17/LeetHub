42d3c10afa299938d7fd648302eb24fa6aa7f797
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-05-25 19:41:54 +0530

    Added solution - LeetHub

--- Stock buy and sell - GFG/stock-buy-and-sell.java
+++ Stock buy and sell - GFG/stock-buy-and-sell.java
@@ -44,16 +44,21 @@ class Solution{
     //Function to find the days of buying and selling stock for max profit.
     ArrayList<ArrayList<Integer> > stockBuySell(int A[], int n) {
         // code here
-        ArrayList<ArrayList<Integer> > ans = new ArrayList<ArrayList<Integer>>();
-        for(int i = 0 ; i < n-1; i++){
+    ArrayList<ArrayList<Integer> > ans = new ArrayList<ArrayList<Integer>>();
+    for(int i = 0 ; i < n-1 ;i++){
+       
+      
             if(A[i] < A[i+1]){
-                ArrayList<Integer> x = new ArrayList<>();
-                x.add(i);
-                x.add(i+1);
-                ans.add(x);
+                ArrayList<Integer> ans2 = new ArrayList<>();
+                ans2.add(i);
+                ans2.add(i+1);
+                ans.add(ans2);
             }
-        }
-        return ans;
+        
+        
     }
+    return ans;
+    }
+    
     
 }
