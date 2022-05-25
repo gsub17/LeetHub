75f12decb3bea9c26fe9d76b7b7b5c69d7a4971c
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-05-25 15:54:39 +0530

    Time: 546 ms (13.20%), Space: 42.8 MB (37.17%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,34 +1,29 @@
 class Solution {
     public int countSubstrings(String s) {
+        int count = 0;
+        for(int i = 0 ; i < s.length() ;i++){
+            for(int j = i+1 ; j <= s.length() ; j++){
+                String temp = s.substring(i ,j );
+                int s1 = 0;
+                int e1 = temp.length()-1;
+                boolean check = false;
+                while(s1 < e1){
+                    char x = temp.charAt(s1);
+                    char y = temp.charAt(e1);
+                    if(x != y){
+                        check = true;
+                        break;
+                    }
+                    s1++;
+                    e1--;
+                }
+                if(!check){
+                   // System.out.println(temp);
+                    count++;
+                }
+            }
+        }
+        return count;
         
-       
-        ArrayList<String> ans = new ArrayList<>();
-        ans = substring(s ,ans );
-        return ans.size();
     }
-    
-   public ArrayList<String> substring(String s , ArrayList<String> ans){
-       for(int i = 0 ; i < s.length() ; i++){
-           for(int j = i+1 ; j <= s.length() ; j++){
-              String n1 = s.substring(i , j);
-              if(palindrome(n1)){
-                  ans.add(n1);
-              }
-           }
-       }
-       return ans;
-   }
-    
-   public boolean palindrome(String n1){
-       int i = 0 ;
-       int j = n1.length() -1;
-       while(i < j){
-           if(n1.charAt(i) != n1.charAt(j)){
-               return false;
-           }
-           i++;
-           j--;
-       }
-       return true;
-   }
 }
