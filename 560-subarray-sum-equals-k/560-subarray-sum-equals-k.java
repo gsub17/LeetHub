b0e62d3fd7cd0e6340bf1028148faf4482bfe6b5
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-05-25 22:26:00 +0530

    Time: 21 ms (80.90%), Space: 46.8 MB (69.47%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,37 +1,19 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        //below comment section is naive approach
-        // int count = 0;
-        // for(int i = 0 ; i < nums.length - 1 ;i++){
-        //     int sum = nums[i];
-        //     if(sum == k){
-        //         count++;
-        //     }
-        //     for(int j = i+1 ; j < nums.length ; j++){
-        //         sum+= nums[j];
-        //         if(sum == k){
-        //             count++;
-        //         }
-        //     }
-        // }
-        // if(nums[nums.length-1] == k){
-        //     count++;
-        // }
-        // return count;
-        
-       //hashmap
-       HashMap<Integer , Integer> map = new HashMap<>();
-        int sum = 0;
+        HashMap<Integer , Integer> map = new HashMap<>();
         int count = 0;
-        for(int i = 0 ; i < nums.length ;i++){
-            sum+=nums[i];
+        int sum = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            sum += nums[i];
             if(sum == k){
                 count++;
             }
-            if(map.containsKey(sum-k)){
-                count += map.getOrDefault(sum-k , 0);
+            if(map.containsKey(sum - k)){
+                int x = map.getOrDefault(sum-k,0);
+                count+=x;
             }
-            map.put(sum , map.getOrDefault(sum , 0)+1);
+            map.put(sum , map.getOrDefault(sum , 0) + 1);
+            
         }
         return count;
     }
