f00d92758ae02697ebc298d26a6d68004b4b012c
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-05-25 19:22:52 +0530

    Time: 9 ms (5.98%), Space: 46.4 MB (5.17%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,23 +1,36 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
+        
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        ans = permute(nums , 0 , nums.length-1 , ans);
-        return ans;
+        ans = permutation(nums , 0 , nums.length-1,ans);
+      //  Collections.sort(ans);
+        HashSet<List<Integer>> set = new HashSet<>();
+        for(List<Integer> a:ans){
+            set.add(a);
+        }
+        
+        List<List<Integer>> ans2 = new ArrayList<List<Integer>>();
+        for(List<Integer> b:set){
+            ans2.add(b);
+        }
+        return ans2;
+        
+        
     }
     
-    public List<List<Integer>> permute(int[] nums , int s,int e,List<List<Integer>> ans){
+    public List<List<Integer>> permutation(int[] nums , int s , int e , List<List<Integer>> ans){
         if(s == e){
             List<Integer> ans2 = new ArrayList<>();
-            for(int i:nums){
-                ans2.add(i);
+            for(int a:nums){
+                ans2.add(a);
             }
             ans.add(ans2);
             return ans;
         }
         
-        for(int i=s;i<=e;i++ ){
+        for(int i = 0 ; i < nums.length ; i++){
             int[] nums1 = swap(nums , s , i);
-            permute(nums1 , s+1,e,ans);
+            permutation(nums , s+1 , e , ans);
             int[] nums2 = swap(nums , s , i);
         }
         return ans;
