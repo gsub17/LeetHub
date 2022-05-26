5a5ff6a79fe56f95b8492ce97aa3b8a0749cbdab
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-05-26 12:44:07 +0530

    Added solution - LeetHub

--- Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.java
+++ Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.java
@@ -51,35 +51,33 @@ class Solution{
     
    
     // Function for finding maximum and value pair
-    public static int lenOfLongSubarr (int A[], int N, int k) {
+    public static int lenOfLongSubarr (int A[], int N, int K) {
         //Complete the function
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int start = 0 , end = -1;
-        int sum = 0;
-        int len = 0;
-        for(int i = 0 ; i < N ; i++){
-            sum+=A[i];
-            if(sum == k){
-                start = 0;
+        HashMap<Integer ,Integer> map = new HashMap<>();
+        int currsum =0;
+        int start = 0;
+        int end = -1;
+        int size = 0;
+        
+        for(int  i = 0 ; i < N ; i++){
+            currsum+=A[i];
+            if(currsum == K){
+                start = 0 ;
                 end = i;
-                len = Math.max(end - start + 1 , len);
+                size = Math.max(end - start + 1 , size);
+            }
+            if(map.containsKey(currsum - K)){
+                start = map.getOrDefault(currsum - K , 0) +1;
+                end = i;
+                size = Math.max(end - start + 1 , size);
             }
             
-            if(map.containsKey(sum - k)){
-                start = map.get(sum - k)+1;
-                end = i;
-                len = Math.max(end - start + 1 , len);
-            }
-            //might contains duplicate key
-            if(!map.containsKey(sum)){
-                map.put(sum , i);
+            if(!map.containsKey(currsum)){
+                map.put(currsum , i);
             }
             
         }
-        if(end == -1){
-            return 0;
-        }
-        return len;
+        return size;
     }
     
     
