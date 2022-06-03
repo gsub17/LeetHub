c9e0a02cba35b77d99b36a24bb962aa64da17833
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-06-03 20:49:27 +0530

    Added solution - LeetHub

--- Permutations of a given string - GFG/permutations-of-a-given-string.java
+++ Permutations of a given string - GFG/permutations-of-a-given-string.java
@@ -30,25 +30,56 @@ class Solution {
     public List<String> find_permutation(String S) {
         // Code here
         List<String> ans = new ArrayList<>();
-        ans = permutation(S,0 , S.length()-1,ans);
-        Collections.sort(ans);
-        return ans;
+        ans = permute(S , 0 , S.length() - 1 , ans);
+        HashSet<String> set = new HashSet<>();
+        for(String a:ans){
+            set.add(a);
+        }
+        List<String> ans2 = new ArrayList<>();
+        for(String a:set){
+            ans2.add(a);
+        }
+        Collections.sort(ans2);
+        return ans2;
     }
     
-    public List<String> permutation(String S , int left , int right , List<String> ans){
+    // public List<String> permute(String S , int s , int e , List<String> ans){
+    //     if(s == e){
+    //         ans.add(S);
+    //         return ans;
+    //     }
+        
+    //     for(int i = s ; i <= e ; i++){
+    //         String temp1 = swap(S , s , i);
+    //         permute(temp1 , s+1 , e , ans);
+    //         String temp2 = swap(S , s , i);
+    //     }
+    //     return ans;
+    // }
+    
+     public List<String> permute(String S , int left , int right , List<String> ans){
         if(left == right){
             ans.add(S);
             return ans;
         }
         for(int i =left ; i <= right ; i++){
             String s1 = swap(S,left,i);
-            permutation(s1 , left+1,right,ans);
+            permute(s1 , left+1,right,ans);
             String s2 = swap(S,left,i);
         }
         return ans;
     }
     
-    public String swap(String S , int i , int j){
+    // public String swap(String S , int i, int j){
+    //     char[] arr = S.toCharArray();
+    //     char temp = arr[i];
+    //     arr[i] = arr[j];
+    //     arr[j] = temp;
+    //     String p = String.valueOf(arr);
+    //     return p;
+    // }
+    
+     public String swap(String S , int i , int j){
         char temp ;
         char[] arr = S.toCharArray();
         temp = arr[i];
