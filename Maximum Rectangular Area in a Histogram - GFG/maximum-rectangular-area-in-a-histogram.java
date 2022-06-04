4bde35d754f7655b1bff5996b2e3f291a2206741
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-06-04 18:54:27 +0530

    Added solution - LeetHub

--- Maximum Rectangular Area in a Histogram - GFG/maximum-rectangular-area-in-a-histogram.java
+++ Maximum Rectangular Area in a Histogram - GFG/maximum-rectangular-area-in-a-histogram.java
@@ -30,55 +30,45 @@ class Solution
     public static long getMaxArea(long hist[], long n) 
     {
         // your code here
-        
-        int[] prev_smaller = new int[hist.length];
         Stack<Integer> s1 = new Stack<>();
+        int[] prev_smaller = new int[hist.length];
         
-        int index = 0;
-        
-        for(int i = 0 ; i < hist.length ;i++){
-            while( !s1.isEmpty() && hist[s1.peek()] >=  hist[i]){
+        for(int i = 0 ; i < hist.length ; i++){
+            while(!s1.isEmpty() && hist[s1.peek()] >= hist[i]){
                 s1.pop();
             }
             
             if(s1.isEmpty()){
-                prev_smaller[index] = -1;
+                prev_smaller[i] = -1;
             }else{
-                prev_smaller[index] = s1.peek();
+                prev_smaller[i] = s1.peek();
             }
             
-            index++;
             s1.push(i);
         }
         
-        int[] next_smaller = new int[hist.length];
-        Stack<Integer> s2 = new Stack<>();
-        index = hist.length - 1;
         
-        for(int i = hist.length -1 ; i >= 0 ; i--){
+        Stack<Integer> s2 = new Stack<>();
+        int[] next_smaller = new int[hist.length];
+        
+        for(int i = hist.length-1 ; i >= 0 ; i--){
             while(!s2.isEmpty() && hist[s2.peek()] >= hist[i]){
                 s2.pop();
             }
             
             if(s2.isEmpty()){
-                next_smaller[index] = hist.length;
+                next_smaller[i] = hist.length;
             }else{
-                next_smaller[index] = s2.peek();
+                next_smaller[i] = s2.peek();
             }
-         //   System.out.println(next_smaller[index]);
-            index--;
             s2.push(i);
         }
         
-     
-        long ans = 0;
-        for(int i = 0 ; i < hist.length ; i++){
-            long temp = (next_smaller[i] - prev_smaller[i] - 1)*hist[i];
-            ans = Math.max(temp , ans);
+        long max = 0;
+        for(int i = 0  ; i < hist.length ;i++){
+            max = Math.max(max , (next_smaller[i] - prev_smaller[i] - 1)*hist[i]);
         }
-        return ans;
-        
-        
+        return max;
     }
         
 }
