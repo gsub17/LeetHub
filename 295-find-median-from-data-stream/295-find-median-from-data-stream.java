156fb324f047c411ce24124f5b5b3fa869f70a9d
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-06-04 11:04:05 +0530

    Time: 181 ms (48.54%), Space: 126.5 MB (16.19%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.java
@@ -1,36 +1,39 @@
 class MedianFinder {
     
-    PriorityQueue<Integer> max_heap ;
-    PriorityQueue<Integer> min_heap;
+    static PriorityQueue<Integer> max_heap;
+    static PriorityQueue<Integer> min_heap;
+   
 
     public MedianFinder() {
         max_heap = new PriorityQueue<>(Collections.reverseOrder());
         min_heap = new PriorityQueue<>();
     }
     
-    public void addNum(int num) {
-        if(max_heap.isEmpty() || max_heap.peek() > num){
-            max_heap.add(num);
+    public void addNum(int x) {
+           
+        if(max_heap.isEmpty() || max_heap.peek() > x){
+            max_heap.add(x);
         }else{
-            min_heap.add(num);
+            min_heap.add(x);
         }
-        // balancing condition
-        if(max_heap.size() - min_heap.size() == 2){
-            min_heap.add(max_heap.poll());
-        }else if(min_heap.size() - max_heap.size() == 2){
-            max_heap.add(min_heap.poll());
-        }
-        findMedian();
+        
+      if(max_heap.size() - min_heap.size() == 2){
+          min_heap.add(max_heap.poll());
+      }else if(min_heap.size() - max_heap.size() == 2){
+          max_heap.add(min_heap.poll());
+      }
+        
     }
     
     public double findMedian() {
-        if(max_heap.size() == min_heap.size() ){
-            return (double)(max_heap.peek()+min_heap.peek())/2;
+         if(max_heap.size() == min_heap.size()){
+            return (double)(max_heap.peek() + min_heap.peek())/2;
         }else if(max_heap.size() > min_heap.size()){
-            return max_heap.peek();
+            return (double)max_heap.peek();
         }
         
-        return min_heap.peek();
+        return (double) min_heap.peek();
+       
     }
 }
 
