3a4694f5c9a5bd193b6ac46fa5eff06c56faafbc
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-06-08 13:41:01 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (96.24%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
@@ -1,46 +1,46 @@
 class MyQueue {
-    
-    static Stack<Integer> s1;
-    static Stack<Integer> s2;
+    Stack<Integer> in ;
+    Stack<Integer> out ;
 
     public MyQueue() {
-        s1 = new Stack<>();
-        s2 = new Stack<>();
+        in = new Stack<>();
+        out = new Stack<>();
     }
     
     public void push(int x) {
-        s1.push(x);
+        in.push(x);
     }
     
     public int pop() {
-        while(!s1.isEmpty()){
-            s2.push(s1.pop());
+        while(!in.isEmpty()){
+            out.push(in.pop());
         }
-        int x = s2.pop();
         
-        while(!s2.isEmpty()){
-            s1.push(s2.pop());
+        int ans = out.pop();
+        while(!out.isEmpty()){
+            in.push(out.pop());
         }
-        return x;
+        return ans;
     }
     
     public int peek() {
-        while(!s1.isEmpty()){
-            s2.push(s1.pop());
+        while(!in.isEmpty()){
+            out.push(in.pop());
         }
-        int x = s2.peek();
         
-        while(!s2.isEmpty()){
-            s1.push(s2.pop());
+        int ans = out.peek();
+        while(!out.isEmpty()){
+            in.push(out.pop());
         }
-        return x;
+        return ans;
     }
     
     public boolean empty() {
-        if(s1.isEmpty()){
-            return true;
+        if(!in.isEmpty()){
+            return false;
         }
-        return false;
+        
+        return true;
     }
 }
 
