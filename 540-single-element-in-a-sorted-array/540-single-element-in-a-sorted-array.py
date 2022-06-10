c34590c2d156a892017842725651937deaf21e02
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-06-10 18:32:07 +0530

    Time: 267 ms (33.73%), Space: 23.8 MB (12.81%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -1,10 +1,8 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        x = nums[0]
+        ans = nums[0]
         for i in range(1 , len(nums) , 1):
-            x = x ^ nums[i]
-        return x
+            ans = ans ^ nums[i]
+        return ans
             
         
