534dc9cf305bcfd5e5836aa70cb339a56ef7b3e3
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-06-14 18:54:20 +0530

    Time: 10 ms (5.26%), Space: 45.7 MB (11.51%) - LeetHub

--- 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.java
+++ 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.java
@@ -15,25 +15,24 @@
  */
 class Solution {
     public List<Integer> largestValues(TreeNode root) {
-       
-        ArrayList<ArrayList<Integer>> ans1 = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> ans1 = new ArrayList<>();
         Queue<TreeNode> q = new LinkedList<>();
-        ArrayList<Integer> ans2 = new ArrayList<>();
+        PriorityQueue<Integer> ans2 = new PriorityQueue<>(Collections.reverseOrder());
         q.add(root);
         q.add(null);
          if(root == null){
-            return ans2;
+            return ans1;
         }
         while(!q.isEmpty()){
             TreeNode curr = q.poll();
             if(curr == null){
                 if(q.isEmpty()){
-                    ans1.add(ans2);
+                    ans1.add(ans2.poll());
                     break;
                 }
                 q.add(null);
-                ans1.add(ans2);
-                ans2 = new ArrayList<>();
+                ans1.add(ans2.poll());
+                ans2 = new PriorityQueue<>(Collections.reverseOrder());
             }else{
                 ans2.add(curr.val);
                 
@@ -47,12 +46,7 @@ class Solution {
             }
         }
         
-        List<Integer> ans3 = new ArrayList<>();
-        for(ArrayList<Integer> a : ans1){
-            Collections.sort(a);
-            ans3.add(a.get(a.size()-1));
-        }
-        return ans3;
-        
+      
+        return ans1;
     }
 }
