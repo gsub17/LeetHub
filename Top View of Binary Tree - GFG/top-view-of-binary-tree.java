53a6df57e0869aa3b82f1720d89d007564b8c465
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-06-14 23:39:09 +0530

    Added solution - LeetHub

--- Top View of Binary Tree - GFG/top-view-of-binary-tree.java
+++ Top View of Binary Tree - GFG/top-view-of-binary-tree.java
@@ -132,40 +132,40 @@ class Solution
         // add your code
         Queue<Pair> q = new ArrayDeque<>();
         Map<Integer , Integer> map = new TreeMap<>();
-        
-        q.add(new Pair(0,root));
+        q.add(new Pair(0 , root));
         
         while(!q.isEmpty()){
-            Pair curr = q.poll();
             
+            Pair curr = q.poll();
             if(!map.containsKey(curr.hd)){
                 map.put(curr.hd , curr.node.data);
             }
             
             if(curr.node.left != null){
-                q.add(new Pair(curr.hd-1 , curr.node.left));
+                q.add(new Pair(curr.hd-1,curr.node.left));
             }
             
             if(curr.node.right != null){
-               q.add(new Pair(curr.hd+1 , curr.node.right));
+                q.add(new Pair(curr.hd+1,curr.node.right));
             }
+        
         }
         
         ArrayList<Integer> ans = new ArrayList<>();
-        for(Map.Entry<Integer , Integer> entry: map.entrySet()){
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
             ans.add(entry.getValue());
         }
-        return ans;
         
+        return ans;
     }
     
     static class Pair{
-        int hd; // horizontal distance
+        int hd;
         Node node;
         
-        public Pair(int hd , Node node){
-            this.node = node;
+        public  Pair(int hd ,Node node){
             this.hd = hd;
+            this.node = node;
         }
     }
 }
