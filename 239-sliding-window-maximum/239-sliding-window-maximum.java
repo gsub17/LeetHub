4a6bf880b39eafbb184a06a06d3912899c942d49
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-06-20 23:00:35 +0530

    Time: 75 ms (26.53%), Space: 145.6 MB (35.41%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,41 +1,36 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        ArrayDeque<Integer> aq = new ArrayDeque<>();
-        for(int i = 0 ; i < k ;i++){
-            while(!aq.isEmpty() && nums[aq.peekLast()] <= nums[i]){
-                aq.removeLast();
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        for(int i = 0 ; i < k ; i++){
+            while(!q.isEmpty() && nums[q.peekLast()] <= nums[i]){
+                q.removeLast();
             }
-            aq.addLast(i);
+            q.addLast(i);
         }
-        
         ArrayList<Integer> ans = new ArrayList<>();
         
-        for(int i = k ; i < nums.length ; i++){
+        
+        for(int i = k ; i < nums.length ;i++){
             
-            ans.add(nums[aq.peekFirst()]);
+            ans.add(nums[q.peek()]);
             
-            while(!aq.isEmpty() && aq.peekFirst() <= i-k){
-                aq.removeFirst();
+            while(!q.isEmpty() && q.peekFirst() <= i- k){
+                q.removeFirst();
             }
             
-              while(!aq.isEmpty() && nums[aq.peekLast()] <= nums[i]){
-                aq.removeLast();
+            while(!q.isEmpty() && nums[q.peekLast()] <= nums[i]){
+                q.removeLast();
             }
-            aq.addLast(i);
-            
+            q.addLast(i);
             
         }
-        
-        ans.add(nums[aq.peekFirst()]);
-
-        
+        ans.add(nums[q.peek()]);
         int[] ans2 = new int[ans.size()];
         int count = 0;
-        for(int a:ans){
-            ans2[count] = a;
+        for(int i:ans){
+            ans2[count] = i;
             count++;
         }
-        
         return ans2;
         
     }
