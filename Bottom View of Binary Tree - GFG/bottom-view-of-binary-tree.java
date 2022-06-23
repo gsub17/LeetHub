181f88fd02a93d14d1f83edb3305d55b6616aa77
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-06-23 16:43:25 +0530

    Added solution - LeetHub

--- Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.java
+++ Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.java
@@ -122,30 +122,27 @@ class Solution
     public ArrayList <Integer> bottomView(Node root)
     {
         // Code here
-        Queue<Pair> q = new ArrayDeque<>();
-        Map<Integer,Integer> map = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<>();
+        Map<Integer,Integer> map  = new TreeMap<>();
         q.add(new Pair(0 , root));
         
         while(!q.isEmpty()){
             Pair curr = q.poll();
-         //   if(!map.containsKey(curr.hd)){
+           // if(!map.containsKey(curr.hd)){
                 map.put(curr.hd , curr.node.data);
-           // }
+          //  }
             
-            if(curr.node.left != null){
-                q.add(new Pair(curr.hd-1 , curr.node.left));
-            }
+            if(curr.node.left != null) q.add(new Pair(curr.hd -1 , curr.node.left));
+            if(curr.node.right != null) q.add(new Pair(curr.hd +1 , curr.node.right));
             
-            if(curr.node.right != null){
-                q.add(new Pair(curr.hd + 1 , curr.node.right));
-            }
-        }
-        ArrayList<Integer> ans = new ArrayList<>();
-        for(Map.Entry <Integer,Integer> entry : map.entrySet()){
-            ans.add(entry.getValue());
         }
         
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(Map.Entry <Integer,Integer> entry:map.entrySet()){
+            ans.add(entry.getValue());
+        }
         return ans;
+        
         
     }
     
@@ -157,5 +154,6 @@ class Solution
             this.hd = hd;
             this.node = node;
         }
+        
     }
 }
