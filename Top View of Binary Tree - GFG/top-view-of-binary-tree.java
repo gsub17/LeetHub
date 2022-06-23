b64d18286cd84d7b3ec77f07d6775c6f08871323
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-06-23 16:35:20 +0530

    Added solution - LeetHub

--- Top View of Binary Tree - GFG/top-view-of-binary-tree.java
+++ Top View of Binary Tree - GFG/top-view-of-binary-tree.java
@@ -130,11 +130,10 @@ class Solution
     static ArrayList<Integer> topView(Node root)
     {
         // add your code
-        ArrayList<Integer> ans = new ArrayList<>();
         Queue<Pair> q = new ArrayDeque<>();
-        Map<Integer , Integer> map = new TreeMap<>();
-        q.add(new Pair(0 , root));
+        Map<Integer, Integer> map = new TreeMap<>();
         
+        q.add(new Pair(0,root));
         while(!q.isEmpty()){
             Pair curr = q.poll();
             
@@ -142,19 +141,14 @@ class Solution
                 map.put(curr.hd , curr.node.data);
             }
             
-            if(curr.node.left != null){
-                q.add(new Pair(curr.hd-1 ,curr.node.left ));
-            }
-            
-            if(curr.node.right != null){
-                q.add(new Pair(curr.hd +1 , curr.node.right));
-            }
+            if(curr.node.left != null) q.add(new Pair(curr.hd -1 , curr.node.left));
+            if(curr.node.right != null) q.add(new Pair(curr.hd +1 , curr.node.right));
         }
         
+        ArrayList<Integer> ans = new ArrayList<>();
         for(Map.Entry <Integer,Integer> entry:map.entrySet()){
             ans.add(entry.getValue());
         }
-        
         return ans;
         
     }
@@ -163,7 +157,7 @@ class Solution
         int hd;
         Node node;
         
-        public Pair(int hd , Node node){
+        public  Pair(int hd , Node node){
             this.hd = hd;
             this.node = node;
         }
