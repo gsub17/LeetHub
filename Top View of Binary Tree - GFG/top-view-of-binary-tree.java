0538e0e80e6f10cdb5f615f7ffbcc70a74abdc87
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-06-27 11:03:28 +0530

    Added solution - LeetHub

--- Top View of Binary Tree - GFG/top-view-of-binary-tree.java
+++ Top View of Binary Tree - GFG/top-view-of-binary-tree.java
@@ -129,11 +129,11 @@ class Solution
     //from left to right in Binary Tree.
     static ArrayList<Integer> topView(Node root)
     {
-        // add your code
         Queue<Pair> q = new LinkedList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
         Map<Integer,Integer> map = new TreeMap<>();
-        q.add(new Pair(0 , root));
         
+        q.add(new Pair(0,root));
         while(!q.isEmpty()){
             Pair curr = q.poll();
             
@@ -141,19 +141,24 @@ class Solution
                 map.put(curr.hd , curr.node.data);
             }
             
-            if(curr.node.left != null) q.add(new Pair(curr.hd -1 , curr.node.left));
-            if(curr.node.right != null) q.add(new Pair(curr.hd + 1 , curr.node.right));
+            if(curr.node.left != null){
+                q.add(new Pair(curr.hd-1 , curr.node.left));
+            }
+            
+            if(curr.node.right != null){
+                q.add(new Pair(curr.hd + 1 , curr.node.right));
+            }
         }
         
-        ArrayList<Integer> ans = new ArrayList<>();
         for(Map.Entry<Integer,Integer> entry:map.entrySet()){
             ans.add(entry.getValue());
         }
         return ans;
         
+        
     }
     
-    public static  class Pair{
+    static class Pair{
         int hd;
         Node node;
         
