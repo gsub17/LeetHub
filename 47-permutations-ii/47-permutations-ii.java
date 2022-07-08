1542c42770437499572bcffd8491fdf9c2b3a260
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-07-08 19:23:47 +0530

    Time: 88 ms (12.65%), Space: 56 MB (7.17%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,43 +1,41 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> ans1 = new ArrayList<List<Integer>>();
-        
-        ans1 = permute(nums , 0 , nums.length-1 ,ans1);
+        permute(nums , ans1 , 0 , nums.length - 1);
         HashSet<List<Integer>> set = new HashSet<>();
-        
-        for(List<Integer> a:ans1){
-            set.add(a);
+        for(List<Integer> i:ans1){
+            set.add(i);
         }
         
-        List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        for(List<Integer> a:set){
-            ans.add(a);
-        }
-        
-        return ans;
-    }
-    
-    public List<List<Integer>> permute(int[] nums , int s , int e , List<List<Integer>> ans1){
-        if(s == e){
-            List<Integer> temp = new ArrayList<>();
-            for(int a:nums){
-            temp.add(a);
-            }
-            ans1.add(temp);
-        }
-        
-        for(int i = s ; i <= e ; i++){
-            int[] nums1 = swap(nums , s , i);
-            permute(nums1 , s+1 , e , ans1);
-            int[] nums2 = swap(nums , s , i);
+        ans1 =  new ArrayList<List<Integer>>();;
+        for(List<Integer> i:set){
+            ans1.add(i);
         }
         return ans1;
     }
     
-    public int[] swap(int[] nums , int s , int i){
-        int temp = nums[s];
-        nums[s] = nums[i];
-        nums[i] = temp;
+    public void permute(int[] nums , List<List<Integer>> ans1 , int s , int e){
+        if(s == e){
+            List<Integer> ans2 = new ArrayList<>();
+            for(int i:nums){
+                ans2.add(i);
+            }
+            ans1.add(ans2);
+            return;
+        }
+        
+        for(int i = s ; i <= e;i++){
+            int[] nums1 = swap(nums , s , i);
+            permute(nums1 , ans1 , s + 1 , e);
+            int[] nums2 = swap(nums , s , i);
+        }
+        return;
+    }
+    
+    public int[] swap(int[] nums , int i , int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
         return nums;
     }
 }
