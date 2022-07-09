37ddffe92a59ec86449e7fc2696bd1ad6d5df5b0
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-07-09 20:35:54 +0530

    Added solution - LeetHub

--- Infix to Postfix - GFG/infix-to-postfix.java
+++ Infix to Postfix - GFG/infix-to-postfix.java
@@ -21,45 +21,46 @@ class Solution {
     // Function to convert an infix expression to a postfix expression.
     public static String infixToPostfix(String exp) {
         // Your code here
-        Stack<Character> s = new Stack<>();
         String ans = "";
+        char[] arr = exp.toCharArray();
+        Stack<Character> s = new Stack<>();
         
         for(int i = 0 ; i < exp.length() ; i++){
-            char x = exp.charAt(i);
+            char c = arr[i];
             
-            if(Character.isLetterOrDigit(x)){
-                ans+=x;
-            }else if(x  == '('){
-                s.push(x);
-            }else if(x == ')'){
-                while(!s.isEmpty() && s.peek() != '('){
-                    ans+=s.pop();
-                }
-                s.pop();
+            if(Character.isLetterOrDigit(c)){
+                ans+=c;
             }else{
-                while(!s.isEmpty() && precedence(x) <= precedence(s.peek())){
-                    ans+=s.pop();
+                if(c == '('){
+                    s.push(c);
+                }else if(c == ')'){
+                    while(!s.isEmpty() && s.peek() != '('){
+                        ans += s.pop();
+                    }
+                    s.pop();
+                }else{
+                    while(!s.isEmpty() && priority(s.peek()) >= priority(c)){
+                        ans += s.pop();
+                    }
+                    s.push(c);
                 }
-                s.push(x);
             }
         }
-        
         while(!s.isEmpty()){
-            ans+=s.pop();
+            ans += s.pop();
         }
         return ans;
-        
     }
     
-    public static int precedence(char c){
+    public static int priority(char c){
         switch(c){
-            case '+':
-            case '-':
+            case'+':
+            case'-':
                 return 1;
-            case '*':
-            case '/':
+            case'/':
+            case'*':
                 return 2;
-            case '^':
+            case'^':
                 return 3;
         }
         return -1;
