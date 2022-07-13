1e097858ccda8c46db77482b28c92218a05e005d
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-07-13 11:36:09 +0530

    Added solution - LeetHub

--- Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.java
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.java
@@ -52,59 +52,59 @@ class DriverClass
 
 
 //User function Template for Java
-class  Pair implements Comparable<Pair>{
-        int wt;
-        int v;
-         Pair(int v , int wt){
-            this.v = v;
-            this.wt = wt;
-        }
-        
-        public int compareTo(Pair that){
-            return this.wt - that.wt;
-        }
+class Pair implements Comparable<Pair>{
+    int v;
+    int wt;
+    
+    Pair(int v , int wt){
+        this.v = v;
+        this.wt = wt;
     }
+    
+    public int compareTo(Pair that){
+        return this.wt-that.wt;
+    }
+}
+
 
 class Solution
 {
     //Function to find the shortest distance of all the vertices
     //from the source vertex S.
-
-    
     static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S)
     {
         // Write your code here
-        boolean[] visited = new boolean[V];
         PriorityQueue<Pair> q = new PriorityQueue<>();
-        q.add(new Pair(S,0));
+        boolean[] visited = new boolean[V];
         
         int[] ans = new int[V];
         Arrays.fill(ans,1000000);
         ans[S] = 0;
         
+        q.add(new Pair(S,0));
+        
         while(!q.isEmpty()){
             Pair curr = q.poll();
-            int u = curr.v;
-            int wt = curr.wt;
+            int weight = curr.wt;
+            int vertex = curr.v;
             
-            if(visited[u]){
+             if(visited[vertex]){
                 continue;
             }
             
-          //  ans+=wt;
-            visited[u] = true;
-            ArrayList<ArrayList<Integer>>  neighbour = adj.get(u);
+            visited[vertex] = true;
+            ArrayList<ArrayList<Integer>> neighbours = adj.get(vertex);
+           
+           for(ArrayList<Integer> list:neighbours){
+               int v = list.get(0);
+               int wt = list.get(1);
+               
+               if(ans[v] > wt + ans[vertex]){
+                   ans[v] = ans[vertex] + wt;
+                   q.add(new Pair(v , ans[v]));
+               }
+           }
             
-            for(ArrayList<Integer> list:neighbour){
-                int vertex = list.get(0);
-                int weight = list.get(1);
-                
-                if(ans[vertex] > ans[u] + weight){
-                   // System.out.println(ans[u] +" " + wt);
-                    ans[vertex] = ans[u] + weight;
-                    q.add(new Pair(vertex , ans[vertex]));
-                }
-            }
             
         }
         return ans;
