4d89a0620a86728d4de383dc9f84acc083540a4c
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2022-07-16 00:39:17 +0530

    Added solution - LeetHub

--- Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.java
+++ Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.java
@@ -114,6 +114,15 @@ class GfG {
 
 
 //User function Template for Java
+class Pair{
+    Node node;
+    int hd;
+    
+    public Pair(Node node , int hd){
+        this.node = node;
+        this.hd = hd;
+    }
+}
 
 
 class Solution
@@ -123,37 +132,27 @@ class Solution
     {
         // Code here
         Queue<Pair> q = new LinkedList<>();
-        Map<Integer,Integer> map  = new TreeMap<>();
-        q.add(new Pair(0 , root));
+        Map<Integer,Integer> map = new TreeMap<>();
         
+        q.add(new Pair(root,0));
+        
+        ArrayList<Integer> ans = new ArrayList<>();
         while(!q.isEmpty()){
             Pair curr = q.poll();
-           // if(!map.containsKey(curr.hd)){
+            
+        //    if(!map.containsKey(curr.hd)){
                 map.put(curr.hd , curr.node.data);
           //  }
             
-            if(curr.node.left != null) q.add(new Pair(curr.hd -1 , curr.node.left));
-            if(curr.node.right != null) q.add(new Pair(curr.hd +1 , curr.node.right));
+            if(curr.node.left != null) q.add(new Pair(curr.node.left , curr.hd-1));
+            if(curr.node.right != null) q.add(new Pair(curr.node.right , curr.hd+1));
             
         }
         
-        ArrayList<Integer> ans = new ArrayList<>();
-        for(Map.Entry <Integer,Integer> entry:map.entrySet()){
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
             ans.add(entry.getValue());
         }
         return ans;
-        
-        
-    }
-    
-    public class Pair{
-        int hd;
-        Node node;
-        
-        public Pair(int hd , Node node){
-            this.hd = hd;
-            this.node = node;
-        }
         
     }
 }
